2dc95b819ffc54d42efbd05218c7c60e15af0a05
Mohammed Zoher Guniem <2918474@uis.no>
2020-07-09 14:01:53 +0200

    Updates

---
+++ precache-manifest.e35b6c64ddbc1fa3356c4f9cd4b4a60c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfb1d5c058d967f35fb169432aa22b41",
+    "url": "/math-genius/index.html"
+  },
+  {
+    "revision": "afafd25a6147e3aee15f",
+    "url": "/math-genius/static/css/main.7401b0d0.chunk.css"
+  },
+  {
+    "revision": "ee7568eb08f93028fd69",
+    "url": "/math-genius/static/js/2.8384a00e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/math-genius/static/js/2.8384a00e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afafd25a6147e3aee15f",
+    "url": "/math-genius/static/js/main.ae829adb.chunk.js"
+  },
+  {
+    "revision": "c7b3b3f98e4b4b4c2596",
+    "url": "/math-genius/static/js/runtime-main.0e350d83.js"
+  }
+]);
